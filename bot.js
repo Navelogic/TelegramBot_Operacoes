92ac9659653db04499a65935fcae19769562a12e
Arian Weslley <weslleysantoszbd@gmail.com>
2022-09-22 17:12:44 -0300

    Celsius Para Fahrenheit

---
+++ bot.js
@@ -0,0 +1,26 @@
+const env = require('./.env');
+const { Telegraf, Context } = require('telegraf');
+const bot = new Telegraf(env.token);
+
+bot.start(async ctx =>{
+  //Capturando dados do USER
+  const nome = ctx.update.message.from.first_name;
+  const idUser = ctx.update.message.from.id;
+  const userName = ctx.update.message.from.username;
+  const lang = ctx.update.message.from.language_code;
+
+ await ctx.replyWithHTML(`Olá <b>${userName}</b>. Eu sou o <b>CALCULATOR</b>, um bot de formas matemáticas!`);
+ await ctx.reply(`Vamos começar fazendo uma conversão de Celsius para Fahrenheit!`);
+ bot.on('text',async ctx =>{
+  const celsius = ctx.update.message.text;
+  const calculo = ((celsius)*9/5) + 32;
+  const celsiusParaFahrenheit = calculo.toLocaleString('pt-BR');
+  ctx.reply(celsiusParaFahrenheit);
+ });
+});
+
+
+
+
+
+bot.startPolling();
